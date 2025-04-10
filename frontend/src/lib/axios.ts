b76ad0198727ae219f356fb36038fd238c890517
numanjvd82 <nomanmani62@gmail.com>
2025-04-10 18:45:37 +0500

    frontend ready for deploy

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8000";
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+const API_BASE_URL = BACKEND_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
